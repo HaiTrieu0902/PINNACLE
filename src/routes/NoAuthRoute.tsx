ef45e8d933576414ecdaea7ef1fd9e8d4e3bc39d
Bùi Hải Triều <trieuql159@gmail.com>
2023-07-07 14:30:24 +0700

    "TLUY"

--- src/routes/NoAuthRoute.tsx
+++ src/routes/NoAuthRoute.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from 'react-router-dom';
-const PrivateRoutes = () => {
+const NoAuthRoute = () => {
     const authToken = localStorage.getItem('accessToken');
     return authToken ? <Outlet /> : <Navigate to={'/login'} replace />;
 };
 
-export default PrivateRoutes;
+export default NoAuthRoute;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,8 @@
 import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ROUTES } from '../configs/routes';
+import NoAuthRoute from './NoAuthRoute';
+import PrivateRoutes from './PrivateRoute';
 const LoginPage = lazy(() => import('../modules/page/LoginPage'));
 const HomePage = lazy(() => import('../pages/HomePage'));
 const DashBroadPage = lazy(() => import('../pages/dashbroad/DashBroadPage'));
@@ -11,11 +13,16 @@ export const RoutesConfig = () => {
         <>
             <Suspense fallback={<div>Loading.....</div>}>
                 <Routes>
+                    <Route element={<NoAuthRoute />}>
+                        <Route path={ROUTES.home} Component={HomePage} />
+                        <Route path={ROUTES.dashbroad} Component={DashBroadPage} />
+                        <Route path={ROUTES.release} Component={RelasePage} />
+                    </Route>
+
+                    <Route element={<PrivateRoutes />}>
+                        <Route path={ROUTES.login} Component={LoginPage} />
+                    </Route>
                     <Route path={ROUTES.mainpage} Component={LoginPage} />
-                    <Route path={ROUTES.login} Component={LoginPage} />
-                    <Route path={ROUTES.home} Component={HomePage} />
-                    <Route path={ROUTES.dashbroad} Component={DashBroadPage} />
-                    <Route path={ROUTES.release} Component={RelasePage} />
                 </Routes>
             </Suspense>
         </>
